145511a8c4ab00e9c85fdac0d493b98b50039524
baccouchee <omar.baccouche.10@gmail.com>
2021-03-10 01:44:54 +0100

    commit 2

--- database/mongodb.js
+++ database/mongodb.js
@@ -3,4 +3,4 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/books', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => console.log('Connecter a la base de donnée'))
+}).then(() => console.log('Connecter a la base de donnée port :3000'))
